d15d1d4453331d026cac0eabe2780dadc2306dab
wallasf <wallasf@hotmail.com>
2026-03-07 14:26:16 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+from pygame.examples.go_over_there import screen
+
+print("Setup Start")
+pygame.init()
+
+window = pygame.display.set_mode(size=(600, 480))
+
+print("Setup End")
+
+print("Loop Start")
+while true:
+    pass
+    # check for all events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:  # close window
+            pygame.quit()
+            quit()  # end pygame
